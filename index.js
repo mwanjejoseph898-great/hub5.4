79e3430eed4367770f94ba615ff21ce3b6ef248f
mwanjejoseph898-great <mwanjejoseph898@gmail.com>
2026-05-16 19:40:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+window.alert("BOOK NOW AND SECURE YOUR SLICE");
+
+function myfunction1(){
+    document.getElementById("head1").style.color ="red";
+   
+}
+function return1(){
+    document.getElementById("head1").style.color ='';
+}
+function myfunction2(){
+    document.getElementById("head2").style.color ="yellow";
+   
+}
+function return2(){
+    document.getElementById("head2").style.color ='';
+}
+function myfunction3(){
+    document.getElementById("head3").style.color ="blue";
+   
+}
+function return3(){
+    document.getElementById("head3").style.color ='';
+}
+
+function clickme(){
+    document.getElementById("mo").innerHTML="WE ALSO HAVE BAKING CLASSES";
+}
